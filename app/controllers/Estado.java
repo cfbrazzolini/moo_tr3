74601c784f31b329e1dd5b884752eb6b6941fb1b
Lucas Neto Moreira <lucasnbsb@gmail.com>
2014-06-19 23:07:08 -0300

    Criadas as controladoras, atentar para o erro no conf/routes

---
+++ app/controllers/Estado.java
@@ -0,0 +1,12 @@
+package controllers;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+
+public class Estado extends Controller {
+
+  public static Result index(String uf, String serie_inicial, String serie_final, String op,
+      String args) {
+    return TODO;
+  }
+}
