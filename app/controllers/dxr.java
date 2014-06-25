0cab305a67f76f94fd64dee02e9eb98bdd1a4c9a
Lucas Neto Moreira <lucasnbsb@gmail.com>
2014-06-25 16:53:55 -0300

    novas controladoras, módulo do mongo ligado, criar db com o nome dadosinep

---
+++ app/controllers/dxr.java
@@ -0,0 +1,12 @@
+package controllers;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+
+public class dxr extends Controller {
+
+  public static Result index(String area, String code, String serie_inicial, String serie_final,
+      String params) {
+    return TODO;
+  }
+}
---
+++ app/controllers/hxr.java
@@ -0,0 +1,12 @@
+package controllers;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+
+public class hxr extends Controller {
+
+  public static Result index(String area, String code, String serie_inicial, String serie_final,
+      String params) {
+    return TODO;
+  }
+}
