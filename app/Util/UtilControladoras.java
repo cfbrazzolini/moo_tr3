a7736ba6367d88574a2512c184b1410680cd996f
Lucas Neto Moreira <lucasnbsb@gmail.com>
2014-06-26 22:48:51 -0300

    modificando levemente a arquitetura, adicionando uma querie de teste para o parâmetro de série inicial e final

--- app/Util/UtilControladoras.java
+++ app/Util/UtilControladoras.java
@@ -1,4 +1,4 @@
-package controllers;
+package Util;
 
 import java.util.ArrayList;
 import java.util.Arrays;
--- app/controllers/dxr.java
+++ app/controllers/dxr.java
@@ -2,11 +2,23 @@ package controllers;
 
 import java.util.List;
 
+import play.mvc.Controller;
 import play.mvc.Result;
+import Util.UtilControladoras;
 
+import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 
-public class dxr extends UtilControladoras {
+public class dxr extends Controller {
+  private static List<String> montarRangeTdi(String serie_inicial, int serie_final) {
+    return UtilControladoras.montarRange("tdi",
+        UtilControladoras.serieParametro.indexOf(serie_inicial), serie_final);
+  }
+
+  private static List<String> montarRangeTxr(String code, int serie_final) {
+    return UtilControladoras.montarRange("txr", UtilControladoras.serieParametro.indexOf(code),
+        serie_final);
+  }
 
   public static Result index(String area, String code, String serie_inicial, String serie_final,
       String params) {
@@ -15,11 +27,28 @@ public class dxr extends UtilControladoras {
       return TODO;
     } else {
       // monta uma lista com as colunas que serão utilizadas no calculo do coeficiente de correlação
-      if (serie_final == null && serieParametro.indexOf(code) != -1) {
-        List<String> serieRangeTdi = montarRange("tdi", serieParametro.indexOf(code), SERIE_LIMITE);
-        List<String> serieRangeTxr = montarRange("txr", serieParametro.indexOf(code), SERIE_LIMITE);
+      List<String> serieTdi = null;
+      List<String> serieRangeTxr = null;
+      if (serie_final == null && UtilControladoras.serieParametro.indexOf(serie_inicial) != -1) {
+        serieTdi = montarRangeTdi(serie_inicial, UtilControladoras.SERIE_LIMITE);
+        serieRangeTxr = montarRangeTxr(serie_inicial, UtilControladoras.SERIE_LIMITE);
+      } else if (UtilControladoras.serieParametro.indexOf(serie_inicial) != -1
+          && UtilControladoras.serieParametro.indexOf(serie_final) != -1) {
+        serieTdi = montarRangeTdi(serie_inicial, UtilControladoras.SERIE_LIMITE);
+        serieRangeTxr = montarRangeTxr(serie_inicial, UtilControladoras.SERIE_LIMITE);
+      } else {
+        throw new RuntimeException("serie fora dos limites");
       }
+      // ----------------------------------------------------------------------------------------------
 
+      BasicDBObject query = new BasicDBObject();
+      for (String serie : serieTdi) {
+        // buscar todas as colunas das séries selecionadas que não tiverem o valor -- (nulo)
+        query.append(serie, new BasicDBObject("$ne", "--"));
+
+        // buscar todas as colunas das séries selecionadas
+        // query.append(serie, "");
+      }
     }
     return TODO;
   }
